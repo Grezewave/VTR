0e9b2abea8151a75893a8be2ae624b2b00a801d2
Vitor <Vitor.P.555@gmail.com>
2019-12-13 20:25:03 -0300

    VTR old version linux

---
+++ VTR1.0Ubuntu/Source/Ufunct.py
@@ -0,0 +1,19 @@
+import os
+
+def TMAlign(protein1,protein2):
+    if (not(os.path.exists("tmalign.exe"))):
+        os.system("g++ TMAlign.cpp -o tmalign")
+        print("TMAlign compilado!"))
+    path = protein1[-8:-4] + "x" + protein2[-8:-4] + "_align"
+    os.system("mkdir ../Data/" + path)
+    callalign = "./tmalign " + protein1 + " " + protein2 + " -o " + "../Data/" + path + "/" + protein1[-8:-4]
+    os.system(callalign)
+    return(path)
+
+def create_dir(rtt_path,stc_path):
+    folder = rtt_path[-15:-11] + "_x_" + stc_path[-8:-4]
+    pmlname = "rm -r ../Plots/" + folder + " /s /q"
+    os.system(pmlname)
+    pmlname = "mkdir ../Plots/" + folder
+    os.system(pmlname)
+    return(folder)
