e04fa499ef1cae0f33524d800003c50b443a2121
Vitor <Vitor.P.555@gmail.com>
2020-02-27 14:42:05 -0300

    Update 270220

--- VTR1.3-Windows/Source/OSfunct.py
+++ VTR1.3-Windows/Source/OSfunct.py
@@ -1,4 +1,3 @@
-#Linux function
 import os
 
 def TMAlign(protein1,protein2):
@@ -7,17 +6,18 @@ def TMAlign(protein1,protein2):
         print("TMAlign compilado!")
     path = protein1[protein1.rfind("/")+1:-4] + "x" + protein2[protein2.rfind("/")+1:-4] + "_align"
     if os.path.exists("../Data/" + path):
-        os.system("rm -r ../Data/" + path)
-    os.system("mkdir ../Data/" + path)
-    callalign = "./tmalign " + protein1 + " " + protein2 + " -o " + "../Data/" + path + "/" + protein1[-8:-4]
+        os.system("rd ..\\Data\\" + path + "/s /q")
+    os.system("md ..\\Data\\" + path)
+    callalign = "tmalign " + protein1 + " " + protein2 + " -o " + "../Data/" + path + "/" + protein1[protein1.rfind("/")+1:-4]
     os.system(callalign)
     return(path)
 
 def create_dir(rtt_path,stc_path):
     folder = rtt_path[rtt_path.rfind("/")+1:rtt_path.rfind("_")] + "_x_" + stc_path[stc_path.rfind("/")+1:-4]
     if (os.path.exists("../Plots/" + folder)):
-        pmlname = "rm -r ../Plots/" + folder
+        pmlname = "rd ..\\Plots\\" + folder + "/s /q"
         os.system(pmlname)
-    pmlname = "mkdir ../Plots/" + folder
+    pmlname = "md ..\\Plots\\" + folder
     os.system(pmlname)
     return(folder)
+
--- VTR1.3-Windows/Source/VTR.py
+++ VTR1.3-Windows/Source/VTR.py
@@ -1,24 +1,24 @@
-#VTR v1.0
+#VTR v1.2 entry for windows
 import os
 while (1):
-     os.system("mkdir ../Results")
-     os.system("mkdir ../Results/Contacts")
-     os.system("mkdir ../Results/Matches")
-     os.system("mkdir ../Plots")
-     os.system("mkdir ../Graphs")
-     os.system("clear")
+     os.system("md ..\\Results")
+     os.system("md ..\\Results\\Contacts")
+     os.system("md ..\\Results\\Matches")
+     os.system("md ..\\Plots")
+     os.system("md ..\\Graphs")
+     os.system("cls")
      os.system("echo ____________________________________________________________\n")
      os.system("echo VTR Geometric v 1.1\n")
      os.system("echo ____________________________________________________________\n")
      os.system("echo Proteins in directory: ")
-     os.system("find ../Data -maxdepth 1 -not -type d")
+     os.system("dir ..\Data /a-d /on")
      os.system("echo  ___________________________________________________________\n")
      rtt_protein = input("Choose the first:")
-     while (not(os.path.exists("../Data/" + rtt_protein + ".pdb"))):
+     while (not(os.path.exists("..\\Data\\" + rtt_protein + ".pdb"))):
           print("This file does not exist...")
           rtt_protein = input("Choose the first:")
      stc_protein = input("Choose the second:")
-     while (not(os.path.exists("../Data/" + stc_protein + ".pdb"))):
+     while (not(os.path.exists("..\\Data\\" + stc_protein + ".pdb"))):
           print("This file does not exist...")
           stc_protein = input("Choose the second:")
      cutoff = input("Choose the cutoff:")
@@ -39,9 +39,12 @@ while (1):
      while (question != "y" and question != "n" and question != "Y" and question != "N"):
           question = input("Do you want the detailed analysys? (y/n)")
      if (question == "y" or question == "Y"):
-          execute = "python3 VTR_Geometric.py ../Data/" + rtt_protein + ".pdb ../Data/" + stc_protein + ".pdb " + cutoff + " " + chainrtt1 + " " + chainrtt2 + " " + chainstc1 + " " + chainstc2 + " d"
+          execute = "python VTR_Geometric.py ../Data/" + rtt_protein + ".pdb ../Data/" + stc_protein + ".pdb " + cutoff + " " + chainrtt1 + " " + chainrtt2 + " " + chainstc1 + " " + chainstc2 + " d"
      else:
-          execute = "python3 VTR_Geometric.py ../Data/" + rtt_protein + ".pdb ../Data/" + stc_protein + ".pdb " + cutoff + " " + chainrtt1 + " " + chainrtt2 + " " + chainstc1 + " " + chainstc2
+          execute = "python VTR_Geometric.py ../Data/" + rtt_protein + ".pdb ../Data/" + stc_protein + ".pdb " + cutoff + " " + chainrtt1 + " " + chainrtt2 + " " + chainstc1 + " " + chainstc2
      os.system(execute)
-     input("Press Enter")
-
+     if (question == "y" or question == "Y"):
+          plot = "pymol -Q ../Plots/c_scale" + rtt_protein + "_x_" + stc_protein + ".pml"
+     else:
+          plot = "pymol -Q ../Plots/default" + rtt_protein + "_x_" + stc_protein + ".pml"
+     os.system(plot)
